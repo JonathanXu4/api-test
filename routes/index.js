648fc39c61ba775cd269f0e8cf889e33fb98ad47
Ultimateko <59599239+Ultimateko@users.noreply.github.com>
2020-11-28 02:49:45 -0800

    copied files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Friends' });
+});
+router.get('/add-friend', function(request, response, next){
+  response.render('add-friend')
 });
 
 module.exports = router;
